0ef9d617227ce94ab4390d7088f82648be2939c0
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-05-03 16:55:22 -0300

    Adicionar arquivos de projeto.

---
+++ ECERCISE 03 - TERNARIA M.07/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ECERCISE_03___TERNARIA_M._07 {
+    class Program {
+        static void Main(string[] args) {
+            double preco = 34.5;
+            double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+            Console.WriteLine(+preco);
+        }
+    }
+}
